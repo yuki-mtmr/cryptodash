fc220bb6df7f0a8cb843ff10baa8fa361418178d
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2020-02-25 09:22:46 +0900

    add WelcomeMessage

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import WelcomeMessage from './WelcomeMessage'
 
-function App() {
-  return (
-    <div> Hello World </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <WelcomeMessage/>
+    );
+  }
 }
 
 export default App;
