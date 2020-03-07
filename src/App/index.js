09e9b9ec1381f0776fb4cb1925a0d0d37d313da2
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2020-03-07 20:34:07 +0900

    add AppProvider and hook up to AppBar

--- src/App/index.js
+++ src/App/index.js
@@ -3,14 +3,17 @@ import './App.css';
 import WelcomeMessage from './WelcomeMessage'
 import AppLayout from './AppLayout';
 import AppBar from './AppBar';
+import {AppProvider} from './AppProvider';
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
-        <AppBar/>
-        <WelcomeMessage />
-        </AppLayout>
+        <AppProvider>
+          <AppBar/>
+          <WelcomeMessage />
+        </AppProvider>
+      </AppLayout>
     );
   }
 }
