6c37db7bbd41685369c138fdb0fa5caa7c302abd
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2020-03-11 19:31:14 +0900

    add topSection coinGrid

--- src/Settings/CoinHeaderGrid.js
+++ src/Settings/CoinHeaderGrid.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import styled from "styled-components";
+import { DeletableTile } from "../Shared/Tile";
 
 export const CoinHeaderGridStyled = styled.div`
   display: grid;
-  grid-template-column: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
   `;
 
 export const CoinSymbol = styled.div`
   justify-self: right;
 `;
 
-export default function ({ name, symbol }) {
+const DeleteIcon = styled.div`
+  justify-self: right;
+  display: none;
+  ${DeletableTile}:hover & {
+    display: block;
+    color: red;
+  }
+`;
+
+export default function ({ name, symbol, topSection }) {
   return <CoinHeaderGridStyled>
     <div> {name} </div>
-    <CoinSymbol> {symbol} </CoinSymbol>
+    {topSection ? (
+      <DeleteIcon> X </DeleteIcon>
+    ) : (<CoinSymbol> {symbol} </CoinSymbol>
+      )}
   </CoinHeaderGridStyled>
 }
